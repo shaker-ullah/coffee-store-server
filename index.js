d543da709bc9ec473925ae092c5f8097875583a9
shaker ullah <shakeru449@gmail.comA>
2024-04-27 12:41:19 +0600

    all added

--- index.js
+++ index.js
@@ -111,7 +111,8 @@ async function run() {
     app.delete('/user/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
-      
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
